2a65bd7acf7a892bd91fce19790a635ebb912f5b
jakeberv <jsb439@cornell.edu>
2026-05-26 14:21:22 -0400

    Extract safe future search runner
    Move the future_lapply plan and thread restoration helper out of searchOptimalConfiguration without changing model fitting or IC arithmetic.
    
    Validation: NOT_CRAN=true Rscript scoped search/helper test bundle passed.

---
+++ R/search-helpers.R
@@ -0,0 +1,52 @@
+bifrost_run_future_lapply_safe <- function(X, FUN, workers, is_rstudio_flag) {
+  thread_vars <- c(
+    "OMP_NUM_THREADS",
+    "OPENBLAS_NUM_THREADS",
+    "MKL_NUM_THREADS",
+    "VECLIB_MAXIMUM_THREADS",
+    "NUMEXPR_NUM_THREADS"
+  )
+  old_threads <- Sys.getenv(thread_vars, unset = NA_character_)
+
+  Sys.setenv(
+    OMP_NUM_THREADS = "1",
+    OPENBLAS_NUM_THREADS = "1",
+    MKL_NUM_THREADS = "1",
+    VECLIB_MAXIMUM_THREADS = "1",
+    NUMEXPR_NUM_THREADS = "1"
+  )
+
+  restore_threads <- function() {
+    for (nm in thread_vars) {
+      val <- old_threads[[nm]]
+      if (is.na(val)) {
+        Sys.unsetenv(nm)
+      } else {
+        do.call(Sys.setenv, setNames(list(val), nm))
+      }
+    }
+  }
+
+  tryCatch(
+    {
+      if (.Platform$OS.type == "unix" &&
+          !identical(Sys.info()[["sysname"]], "SunOS") &&
+          !is_rstudio_flag) {
+        plan(multicore, workers = workers)
+      } else {
+        plan(multisession, workers = workers)
+      }
+
+      future.apply::future_lapply(
+        X,
+        FUN,
+        future.seed = TRUE,
+        future.scheduling = TRUE
+      )
+    },
+    finally = {
+      plan(sequential)
+      restore_threads()
+    }
+  )
+}
--- R/searchOptimalConfiguration.R
+++ R/searchOptimalConfiguration.R
@@ -388,63 +388,9 @@ searchOptimalConfiguration <-
 
     is_rstudio <- identical(Sys.getenv("RSTUDIO"), "1")
 
-    # --- local helper: run a future_lapply with safe plan + capped BLAS/OpenMP threads ---
-    .run_future_lapply_safe <- function(X, FUN, workers, is_rstudio_flag) {
-      .thread_vars <- c(
-        "OMP_NUM_THREADS",
-        "OPENBLAS_NUM_THREADS",
-        "MKL_NUM_THREADS",
-        "VECLIB_MAXIMUM_THREADS",
-        "NUMEXPR_NUM_THREADS"
-      )
-      .old_threads <- Sys.getenv(.thread_vars, unset = NA_character_)
-
-      Sys.setenv(
-        OMP_NUM_THREADS = "1",
-        OPENBLAS_NUM_THREADS = "1",
-        MKL_NUM_THREADS = "1",
-        VECLIB_MAXIMUM_THREADS = "1",
-        NUMEXPR_NUM_THREADS = "1"
-      )
-
-      .restore_threads <- function() {
-        for (nm in .thread_vars) {
-          val <- .old_threads[[nm]]
-          if (is.na(val)) {
-            Sys.unsetenv(nm)
-          } else {
-            do.call(Sys.setenv, setNames(list(val), nm))
-          }
-        }
-      }
-
-      tryCatch(
-        {
-          if (.Platform$OS.type == "unix" &&
-              !identical(Sys.info()[["sysname"]], "SunOS") &&
-              !is_rstudio_flag) {
-            plan(multicore, workers = workers)
-          } else {
-            plan(multisession, workers = workers)
-          }
-
-          future.apply::future_lapply(
-            X,
-            FUN,
-            future.seed = TRUE,
-            future.scheduling = TRUE
-          )
-        },
-        finally = {
-          plan(sequential)
-          .restore_threads()
-        }
-      )
-    }
-
     .progress("%s", "Fitting sub-models in parallel...")
 
-    candidate_results <- .run_future_lapply_safe(
+    candidate_results <- bifrost_run_future_lapply_safe(
       candidate_trees_shifts,
       function(tree) {
         if (IC == "GIC") {
@@ -745,7 +691,7 @@ searchOptimalConfiguration <-
             removeShiftFromTree(best_tree_no_uncertainty, shift_node_number)
           })
 
-          ic_results <- .run_future_lapply_safe(
+          ic_results <- bifrost_run_future_lapply_safe(
             shift_removed_trees,
             function(tree) {
               model_fun <- if (IC == "GIC") fitMvglsAndExtractGIC.formula else fitMvglsAndExtractBIC.formula
